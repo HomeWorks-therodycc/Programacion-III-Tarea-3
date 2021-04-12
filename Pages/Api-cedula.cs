9707196b63d46a33b98df70a64c1715ec84cb5b3
therodycc <therodycc@gmail.com>
2021-04-12 14:00:13 -0400

    first commit

---
+++ Pages/Api-cedula.cs
@@ -0,0 +1,17 @@
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+public class RootCedula
+{
+    public string Cedula { get; set; }
+    public string Nombres { get; set; }
+    public string Apellido1 { get; set; }
+    public string Apellido2 { get; set; }
+    public string FechaNacimiento { get; set; }
+    public bool ok { get; set; }
+    public string foto { get; set; }
+}
+
+
